530fd29ed5c3f8db0a8f21210dfec826cd7c09a6
Hüseyin Bilen <huseyinxbilen@gmail.com>
2022-05-25 00:00:03 +0300

    Static Files Created

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Movie App Created</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
